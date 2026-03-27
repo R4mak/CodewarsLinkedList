dc23896765219e8f3e4690b2c6ff88a51c815dd5
R4mak <99018039+R4mak@users.noreply.github.com>
2026-03-27 17:47:23 +0200

    Add move_node function to move nodes between lists

---
+++ Linked Lists - Move Node/movenodestring.py
@@ -0,0 +1,17 @@
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    if source is None:
+        raise ValueError("Source list cannot be empty")
+    
+    node_to_move = source
+    
+    new_source = source.next
+    
+    node_to_move.next = dest
+    new_dest = node_to_move
+    
+    return Context(new_source, new_dest)
