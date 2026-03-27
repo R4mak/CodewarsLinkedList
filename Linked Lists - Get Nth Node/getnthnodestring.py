68b2820e008c21c47df34b252fac6e58f9a4385f
R4mak <99018039+R4mak@users.noreply.github.com>
2026-03-27 17:44:49 +0200

    Implement get_nth function for linked list

---
+++ Linked Lists - Get Nth Node/getnthnodestring.py
@@ -0,0 +1,17 @@
+from preloaded import Node
+
+def get_nth(node, index):
+    if index < 0 or node is None:
+        raise ValueError("Invalid index or empty list")
+    
+    current = node
+    counter = 0
+    
+    while current is not None:
+        if counter == index:
+            return current
+        
+        current = current.next
+        counter += 1
+    
+    raise IndexError("Index out of bounds")
